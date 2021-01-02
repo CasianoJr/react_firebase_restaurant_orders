a0a2d0ff24bec269a5ebc337726d975a7b733f3b
Casiano Fernandez <casiano_fernandez@yahoo.com>
2021-01-02 21:09:08 +0800

    footer

--- src/utils/Footer.js
+++ src/utils/Footer.js
@@ -4,10 +4,9 @@ export default function Footer() {
    return (
       <div className="py-2 mt-5 text-white" style={{ backgroundColor: "rgb(0,0,0,.7)" }}>
          <div className="display-3 text-center font-italic" style={{ fontSize: "3vh" }}>
-            People who think they know everything are a great annoyance to those of us who
-            do.
+            Hello world, let us eat together.
          </div>
-         <div className="text-center">-Isaac Assimov</div>
+         <div className="text-center">casianojrfernandez@gmail.com</div>
       </div>
    );
 }
